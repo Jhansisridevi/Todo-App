8618ad45f66da8de359240d77f3b2d1e489279ec
Jhansi <jhansisridevi@gmail.com>
2023-12-10 21:15:47 -0500

    Todo App starter

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <section className="m-12 h-screen bg-purple-300 flex items-center justify-center text-center p-12 md:p-14 lg:p-16 xl:p-20 ">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus libero, vel dignissimos dolorum porro, eos illo cum voluptatum vero neque sed tempora officia cumque voluptates? Nam id facilis, ratione temporibus aut explicabo, ipsum aliquam fugiat reprehenderit ducimus dolorem minus consequuntur culpa delectus quibusdam quos debitis, eos quod autem iusto voluptatibus. Fuga voluptatibus perferendis eos ea quam. Quasi vitae unde aliquid dolorem inventore commodi ab? Dolore repudiandae autem illo facere, voluptatibus dignissimos deleniti labore atque pariatur perspiciatis quod itaque qui odit totam est ab. Deleniti quia tempora, nostrum eaque minima libero. Natus quisquam excepturi laudantium exercitationem vel ipsa ad rem aperiam?
+      </p>
+     </section>
+    </>
+  )
+}
+
+export default App
+
