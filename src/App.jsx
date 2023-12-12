17b08ab2a97442a58b341dac28666da4ee17cc06
Jhansi <jhansisridevi@gmail.com>
2023-12-12 15:32:30 -0500

    Added initial state values and addtodo function

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,188 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useReducer, useState } from "react";
 
-function App() {
+const initialState = [
+  {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "quis ut nam facilis et officia qui",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: "fugiat veniam minus",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: "et porro tempora",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 5,
+    title: "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 6,
+    title: "qui ullam ratione quibusdam voluptatem quia omnis",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 7,
+    title: "illo expedita consequatur quia in",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 8,
+    title: "quo adipisci enim quam ut ab",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 9,
+    title: "molestiae perspiciatis ipsa",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 10,
+    title: "illo est ratione doloremque quia maiores aut",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 11,
+    title: "vero rerum temporibus dolor",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 12,
+    title: "ipsa repellendus fugit nisi",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 13,
+    title: "et doloremque nulla",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 14,
+    title: "repellendus sunt dolores architecto voluptatum",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 15,
+    title: "ab voluptatum amet voluptas",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 16,
+    title: "accusamus eos facilis sint et aut voluptatem",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 17,
+    title: "quo laboriosam deleniti aut qui",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 18,
+    title: "dolorum est consequatur ea mollitia in culpa",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 19,
+    title: "molestiae ipsa aut voluptatibus pariatur dolor nihil",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 20,
+    title: "ullam nobis libero sapiente ad optio sint",
+    completed: true,
+  },
+];
+
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [
+        {
+          userId: 1, 
+          id: Date.now(), //for test case
+          title: action.payload,
+          completed: false, //initially setting to false 
+        },
+        ...state,
+      ];
+    case "DELETE_TODO":
+      return state.filter((todo) => todo.id !== action.payload); 
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
+  const [newTodo, setNewTodo] = useState("");
+  //adding a new Todo to the list
+  const addTodo = () => {
+    if (newTodo.trim() !== "") {
+      dispatch({ type: "ADD_TODO", payload: newTodo });
+      setNewTodo(""); //setState
+    }
+  };
+  //deleting the existing todo
+  const deleteTodo = (id) => {
+    dispatch({ type: "DELETE_TODO", payload: id });
+  };
 
   return (
-    <>
-     <section className="m-12 h-screen bg-purple-300 flex items-center justify-center text-center p-12 md:p-14 lg:p-16 xl:p-20 ">
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus libero, vel dignissimos dolorum porro, eos illo cum voluptatum vero neque sed tempora officia cumque voluptates? Nam id facilis, ratione temporibus aut explicabo, ipsum aliquam fugiat reprehenderit ducimus dolorem minus consequuntur culpa delectus quibusdam quos debitis, eos quod autem iusto voluptatibus. Fuga voluptatibus perferendis eos ea quam. Quasi vitae unde aliquid dolorem inventore commodi ab? Dolore repudiandae autem illo facere, voluptatibus dignissimos deleniti labore atque pariatur perspiciatis quod itaque qui odit totam est ab. Deleniti quia tempora, nostrum eaque minima libero. Natus quisquam excepturi laudantium exercitationem vel ipsa ad rem aperiam?
-      </p>
-     </section>
-    </>
-  )
-}
+    <div className="min-h-screen bg-gradient-to-br from-yellow-200 to-purple-500 flex items-center justify-center">
+      <div className="bg-gray-50 p-8 rounded text-center shadow-2xl ring-2 ring-gray-300 backdrop-filter backdrop-blur-lg w-full max-w-2xl">
+        {/* <h1 className="text-4xl font-bold mb-4">Todo List</h1> */}
+        <h1 className="text-4xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent font-mono">
+          {" "}
+          Todo List{" "}
+        </h1>
+        <div className="flex mb-4 font-mono">
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            className="border rounded-l px-4 py-2 w-full"
+            placeholder="New Todo"
+          />
+          <button
+            onClick={addTodo}
+            className="bg-purple-700 text-white px-4 py-2 rounded-r-md" //right radius
+          >
+            Add Todo
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
-
+export default App;
