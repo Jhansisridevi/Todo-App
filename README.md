12ccfeab23d698d4a25161432ca8174299598dae
Jhansi <jhansisridevi@gmail.com>
2023-12-13 15:56:49 -0500

    Added README file

--- README.md
+++ README.md
@@ -1,8 +1,47 @@
-# React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+# React Todo App with Tailwind CSS
 
-Currently, two official plugins are available:
+This is a React.js Todo App with Tailwind CSS, featuring a list of todo items. Users can add, edit, mark as complete, and delete todos.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Features
+
+### 1. Todo List
+
+- Each todo item is represented by a string listing a specific activity to be accomplished.
+
+### 2. Todo Item Controls
+
+- **Checkbox:** Indicates whether a todo is "complete" or not.
+
+- **Delete Button:** Removes the todo item from the list. The delete button is disabled unless the todo is complete.
+
+- **Edit Button:** Replaces the todo string with a text input for editing.
+
+- **Save Button:** Saves the new todo string after editing.
+
+### 3. Edit Mode
+
+- When editing a todo, the text input is used to modify the todo content.
+
+- "Delete" and "Edit" buttons are hidden during the edit mode.
+
+- A "Save" button appears during edit mode, allowing users to save changes made to the todo within the text input.
+
+### 4. Add New Todo
+
+- An input element is provided to create and add new todo items.
+
+- New todos are added to the top of the list visually, ensuring the latest todos are displayed first.
+
+## Technologies Used
+
+- React.js
+
+- Tailwind CSS
+
+## Usage
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/Jhansisridevi/Todo-App.git
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useReducer, useState } from "react";
+import { FaPenSquare, FaTrashAlt } from "react-icons/fa";
 
 const initialState = [
   {
@@ -122,21 +123,33 @@ const initialState = [
     completed: true,
   },
 ];
-
+//Later - adding editingindex to inital state
 const todoReducer = (state, action) => {
   switch (action.type) {
     case "ADD_TODO":
       return [
         {
-          userId: 1, 
-          id: Date.now(), //for test case
+          userId: 1,
+          id: Date.now(),
           title: action.payload,
-          completed: false, //initially setting to false 
+          completed: false,
         },
         ...state,
       ];
+    case "EDIT_TODO":
+      return state.map((todo) =>
+        todo.id === action.payload.id
+          ? { ...todo, title: action.payload.title }
+          : todo
+      );
+    case "TOGGLE_COMPLETE":
+      return state.map((todo) =>
+        todo.id === action.payload
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      );
     case "DELETE_TODO":
-      return state.filter((todo) => todo.id !== action.payload); 
+      return state.filter((todo) => todo.id !== action.payload);
     default:
       return state;
   }
@@ -145,26 +158,43 @@ const todoReducer = (state, action) => {
 const App = () => {
   const [todos, dispatch] = useReducer(todoReducer, initialState);
   const [newTodo, setNewTodo] = useState("");
-  //adding a new Todo to the list
+  const [editTodo, setEditTodo] = useState("");
+  const [editingIndex, setEditingIndex] = useState(null); //setting initial index in state
+  //Adding a new todo
   const addTodo = () => {
     if (newTodo.trim() !== "") {
       dispatch({ type: "ADD_TODO", payload: newTodo });
-      setNewTodo(""); //setState
+      setNewTodo("");
     }
   };
-  //deleting the existing todo
+  //Toggling the completed todo
+  const toggleComplete = (id) => {
+    dispatch({ type: "TOGGLE_COMPLETE", payload: id });
+  };
+  //Deleting a todo
   const deleteTodo = (id) => {
     dispatch({ type: "DELETE_TODO", payload: id });
+  };
+  //Edit existing todo
+  const editExisting = (id, title) => {
+    setEditingIndex(id);
+    setEditTodo(title);
+  };
+  //Save the modified todo
+  const saveEdited = (id) => {
+    dispatch({ type: "EDIT_TODO", payload: { id, title: editTodo } });
+    setEditingIndex(null);
   };
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-yellow-200 to-purple-500 flex items-center justify-center">
       <div className="bg-gray-50 p-8 rounded text-center shadow-2xl ring-2 ring-gray-300 backdrop-filter backdrop-blur-lg w-full max-w-2xl">
         {/* <h1 className="text-4xl font-bold mb-4">Todo List</h1> */}
-        <h1 className="text-4xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent font-mono">
+        <h1 className="text-5xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent font-mono">
           {" "}
           Todo List{" "}
         </h1>
+        {/* Input box and button to add new todo */}
         <div className="flex mb-4 font-mono">
           <input
             type="text"
@@ -180,9 +210,70 @@ const App = () => {
             Add Todo
           </button>
         </div>
+        {/* Display existing todo  */}
+        <div>
+          {todos.map((todo) => (
+            <div key={todo.id} className="flex items-center mb-2">
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => toggleComplete(todo.id)}
+                className="mr-2"
+              />
+              {editingIndex === todo.id ? (
+                <>
+                  <input
+                    type="text"
+                    value={editTodo}
+                    onChange={(e) => setEditTodo(e.target.value)}
+                    className="border rounded-l px-4 py-2"
+                  />
+                  <button
+                    onClick={() => saveEdited(todo.id)}
+                    className="bg-green-500 text-white px-4 py-2 rounded-r-md"
+                  >
+                    Save
+                  </button>
+                </>
+              ) : (
+                <>
+                  <span className={todo.completed ? "line-through" : ""}>
+                    {todo.title}
+                  </span>
+                  <button
+                    onClick={() => editExisting(todo.id, todo.title)}
+                    className="ml-auto bg-yellow-500 text-white px-4 py-2 rounded"
+                  >
+                    Edit
+                  </button>
+                  {/* <FontAwesomeIcon icon="fa-solid fa-pen-to-square" /> */}
+                  {/* <FaPenSquare role="button" onClick={() => editExisting(todo.id, todo.title)}
+                    className="ml-auto"
+                  /> */}
+                  <button
+                    onClick={() => deleteTodo(todo.id)}
+                    className={`ml-2 bg-red-500 text-white px-4 py-2 rounded ${
+                      !todo.completed
+                        ? "bg-gray-300 text-gray-500 cursor-not-allowed"
+                        : "bg-red-500 text-white"
+                      //todo.completed ? "cursor-not-allowed" : ""  - functionality changes
+                    }`}
+                    disabled={!todo.completed} //to restrict delete on completed todo
+                  >
+                    Delete
+                  </button>
+                  {/* <FaTrashAlt role="button" onClick={() => deleteTodo(todo.id)}
+                    disabled={todo.completed} /> */}
+                </>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
 export default App;
+
+// Save or Edit and Delete
